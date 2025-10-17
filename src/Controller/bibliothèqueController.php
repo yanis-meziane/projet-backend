362333207308501f01a041a520e9436f477a8bb8
Yanis Meziane <yanismeziane04@icloud.com>
2025-10-17 16:03:13 +0200

    BDD fix

--- src/Controller/bibliothèqueController.php
+++ src/Controller/bibliothèqueController.php
@@ -1,33 +1,38 @@
 <?php
-// src/Controller/LuckyController.php
 namespace App\Controller;
 
-use App\Entity\Product;
-
+use App\Entity\LivresCreation;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Psr\Log\LoggerInterface;
 
 #[Route('/livre')]
 class bibliothèqueController extends AbstractController
 {
-        //Création d'une route pour tester le fonctionnement de base.
+    #[Route('/titre/{titre}/{author}', name:'bookTitle')]
+    public function bookTitle(string $titre, string $author): Response
+    {
+        return $this->render('/title/bookTitle.html.twig', [
+            'title' => $titre, 
+            'author' => $author
+        ]);
+    }
 
-        #[Route('/titre/{titre}/{author}', name:'bookTitle')]
-            public function bookTitle(string $titre, string $author):Response{
-                    return $this->render('/title/bookTitle.html.twig',['title'=>$titre, 'author'=>$author]);
-                }
-
-        // Création des livres 
-
-        #[Route('/create',name:'createBook', methods:['POST'])]
-                public function creationBook(Request $request, EntityManagerInterface $entityManager):Response{
-                        
-                }
-
-            
-    
+    #[Route('/create', name:'createBook')]
+    public function creationBook(EntityManagerInterface $entityManager): Response
+    {
+        $livre = new LivresCreation();
+        $livre->setLivreId(1);
+        $livre->setTitre('Mon premier livre');
+        $livre->setDatePublication(new \DateTime());
+        $livre->setAvailable(true);
+        
+        $entityManager->persist($livre);
+        $entityManager->flush();
+        
+        return $this->render('createLivre/createBooks.html.twig', [
+            'livre' => $livre
+        ]);
+    }
 }
